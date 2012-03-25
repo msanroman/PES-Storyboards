90bdace6a7f1cbe4d510f0428f3dce79054f3356
Jay Kid <cyph3rfox@gmail.com>
2012-03-25 18:11:55 +0200

    algunes seccions amb footer afegit

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,8 @@ $('body nav.navbar:first').html("<div class='navbar-inner'><div class='container
 function inserta_head_contractant() {
 	$('body nav.navbar:first').html('<div class="navbar-inner"><div class="container"><a class="brand" href="index.html">Online freelancing</a><div class="nav-collapse"><ul class="nav"><li><a href="index.html">Inici</a></li><li class="active"><a href="afegir_projecte_form.html">Afegir projecte</a></li><li><a href="my_projects.html">Els meus projectes</a></li><li><a href="#">Sortir</a></li></ul><form class="navbar-search pull-left" action><input type="text" class="search-query span2" placeholder="Cerca"></form></div></div></div>');
 }
+
+function inserta_footer() {
+	$('footer').attr('style','background-color: #2C2C2C; color: white; height: 40px; border-radius: 3px;  padding-top: 8px; padding-right: 30px;');
+	$('footer').html("<div style='float: right;'><p style='font-size: 20px;'>Online freelancing</p><p style='font-size: 9px; margin-top: -10px'>Copyright 2012</p></div>");
+}
