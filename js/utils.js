03be9502e5147e2edd4db3b2bc7f38185d6977cd
Jay Kid <cyph3rfox@gmail.com>
2012-04-12 13:02:08 +0200

    headers improvements

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,12 @@
 ﻿function inserta_head_freelance() {
-$('body nav.navbar:first').html("<div class='navbar-inner'><div class='container'><a class='brand' href='index.html'>Online freelancing</a><div class='nav-collapse'><ul class='nav'><li><a href='index.html'>Inici</a></li><li><a href='project_list.html'>Cercar projectes</a></li><li><a href='my_projects.html'>Els meus projectes</a></li><li><a href='#'>Sortir</a></li></ul><form class='navbar-search pull-left' action><input type='text' class='search-query span2' placeholder='Cerca'></form></div></div></div>");
+$('body nav.navbar:first').html("<div class='navbar-inner'><div class='container'><a class='brand' href='index.html'>Lancing Market</a><div class='nav-collapse'><ul class='nav'><li><a href='index.html'>Inici</a></li><li><a href='project_list.html'>Cercar projectes</a></li><li><a href='my_projects.html'>Els meus projectes</a></li><li><a href='#'>Sortir</a></li></ul><form class='navbar-search pull-left' action><input type='text' class='search-query span2' placeholder='Cerca'></form></div></div></div>");
 }
 
 function inserta_head_contractant() {
-	$('body nav.navbar:first').html('<div class="navbar-inner"><div class="container"><a class="brand" href="index.html">Online freelancing</a><div class="nav-collapse"><ul class="nav"><li><a href="index.html">Inici</a></li><li class="active"><a href="afegir_projecte_form.html">Afegir projecte</a></li><li><a href="my_projects.html">Els meus projectes</a></li><li><a href="#">Sortir</a></li></ul><form class="navbar-search pull-left" action><input type="text" class="search-query span2" placeholder="Cerca"></form></div></div></div>');
+	$('body nav.navbar:first').html('<div class="navbar-inner"><div class="container"><a class="brand" href="index.html">Lancing Market</a><div class="nav-collapse"><ul class="nav"><li><a href="index.html">Inici</a></li><li><a href="afegir_projecte_form.html">Afegir projecte</a></li><li><a href="my_projects.html">Els meus projectes</a></li><li><a href="#">Sortir</a></li></ul><form class="navbar-search pull-left" action><input type="text" class="search-query span2" placeholder="Cerca"></form></div></div></div>');
 }
 
 function inserta_footer() {
 	$('footer').attr('style','background-color: #2C2C2C; color: white; height: 40px; border-radius: 3px;  padding-top: 8px; padding-right: 30px;');
-	$('footer').html("<div style='float: right;'><p style='font-size: 20px;'>Online freelancing</p><p style='font-size: 9px; margin-top: -10px'>Copyright 2012</p></div>");
+	$('footer').html("<div style='float: right;'><p style='font-size: 20px;'>Lancing Market</p><p style='font-size: 9px; margin-top: -10px'>Copyright 2012</p></div>");
 }
